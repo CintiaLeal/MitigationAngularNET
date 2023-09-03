4ddc5d0a56dfb55cfdbbf33657cd096770f92dfb
Cintia Leal <Cintialeal31m@gmail.com>
2023-09-02 21:33:38 -0300

    Mitigation

---
+++ .Net/Prueba2/Prueba2/Controllers/UsuariosController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Prueba2.Models;
+using Microsoft.AspNetCore.Cors;
+
+namespace Prueba2.Controllers
+{
+
+        
+        [EnableCors("ReglasCors")]
+        [Route("usuario")]
+        [ApiController]
+        public class UsuarioController : ControllerBase
+        {
+            [HttpGet]
+            [Route("listar")]
+            public dynamic listaUsuarios()
+            {
+
+                List<Usuario> usu = new List<Usuario>
+            {
+                new Usuario
+                {
+                    Id = 1,
+                    Name = "CINTIA",
+                    Correo = "cintia@gmail.com",
+                    Edad = 23
+                },
+                new Usuario
+                {
+                    Id = 2,
+                    Name = "ANA",
+                    Correo = "ana@gmail.com",
+                    Edad = 23
+                }
+            };
+                return usu;
+
+            }
+
+        
+    }
+}
+
+
+
---
+++ .Net/Prueba2/Prueba2/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace Prueba2.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Correo { get; set; }
+        public int Edad { get; set; }
+        public Usuario() { }
+    }
+
+}
